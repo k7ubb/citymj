82c159719189e246fb49b3f3062efec917a6f4f6
k7ubb <64680945+k7ubb@users.noreply.github.com>
2024-07-02 18:34:21 +0900

    微調整

--- canvaslib.js
+++ canvaslib.js
@@ -27,14 +27,7 @@ class Canvas {
 		}.bind(this));
 		
 		this.#div.addEventListener("mousemove", function(event) {
-			const offset = event.target.getBoundingClientRect();
-			this.#drawonevent(
-				this.#canvas_f.getContext("2d"),
-				this.#width,
-				this.#height,
-				(event.clientX - offset.left) * devicePixelRatio,
-				(event.clientY - offset.top) * devicePixelRatio
-			);
+			this.#drawonevent(event);
 		}.bind(this));
 	};
 	
@@ -67,16 +60,26 @@ class Canvas {
 		ctx.fillRect(0, 0, this.#width, this.#height);
 		this.#update(ctx, this.#width, this.#height);
 		for (let o of this.objects) {
-			if (o.draw) o.draw(ctx);
+			if (o.draw) {
+				o.draw(ctx);
+			}
 		}
 	};
 
-	#drawonevent () {
-		const ctx = this.#canvas_b.getContext("2d");
+	#drawonevent (event) {
+		const offset = event.target.getBoundingClientRect();
+		const ctx = this.#canvas_f.getContext("2d");
 		ctx.clearRect(0, 0, this.#width, this.#height);
-		this.#update(ctx, this.#width, this.#height);
 		for (let o of this.objects) {
-			if (o.drawonevent) o.drawonevent(ctx);
+			if (o.drawonevent) {
+				o.drawonevent(
+					this.#canvas_f.getContext("2d"),
+					this.#width,
+					this.#height,
+					(event.clientX - offset.left) * devicePixelRatio,
+					(event.clientY - offset.top) * devicePixelRatio
+				);
+			}
 		}
 
 	}
