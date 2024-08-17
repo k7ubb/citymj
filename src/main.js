78f261233894d2de1534a908cdf3335b94821959
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-08-17 20:56:12 +0900

    ライブラリを更新して書き直し

---
+++ src/main.js
@@ -0,0 +1,19 @@
+addEventListener("load", () => {
+	const canvas = new BBCanvas(document.getElementById("game"));
+	canvas.x = (x) => x * canvas.width / 16;
+	canvas.y = (y) => y * canvas.height / 9;
+	canvas.invX = (x) => x * 16 / canvas.width;
+	canvas.invY = (y) => y * 9 / canvas.height;
+
+//	menuScene(canvas);
+	gameScene(canvas);
+});
+
+const drawGrid = (canvas, ctx) => {
+	for (let i = 1; i < 16; i++) {
+		ctx.fill(canvas.makePath({rect: [i, 0, ctx.pixel, 9]}), "#ccc");
+	}
+	for (let i = 1; i < 9; i++) {
+		ctx.fill(canvas.makePath({rect: [0, i, 16, ctx.pixel]}), "#ccc");
+	}
+};
