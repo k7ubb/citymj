b046b3dc486febdc23ea9154b1fcc1ca644f4d18
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-09-09 15:42:50 +0900

    ライブラリの機能微調整

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const ctx = ResponsiveCanvas(document.getElementById("game"), 16, 9, true);
 const $ = new ItemsCanvas(ctx, ctx.convert, ctx.convertBack);
+$.screenRect = [0, 0, 16, 9];
 const lineWidth = 0.01;
 ctx.onResize = () => $.update();
 
