c44771c0ad7a8ff4d753adf4b085a065723b5a70
k7ubb <64680945+k7ubb@users.noreply.github.com>
2024-09-23 00:08:22 +0900

    ファイル構成を修正・役描画リファクタ

---
+++ src/logic.js
@@ -0,0 +1,196 @@
+const getCitiesInHand = (hand) => {
+	const cities = [];
+	for (let i = 0; i < hand.length; i++) {
+		for (const city of CITIES) {
+			for (let k = 0; k < city.name.length; k++) {
+				if (hand[i + k]?.character !== city.name[k]) { break; }
+				if (k === city.name.length - 1) {
+					cities.push({
+						...city,
+						position: i,
+						length: city.name.length
+					});
+				}
+			}
+		}
+	}
+	return cities;
+};
+
+const findYaku = ({
+	count,	// あがった順目 (天和: 0)
+	dora,
+	uradora,
+	isRinshan,
+	kans,
+	reachCount,	// リーチした順目 (リーチなし: -1, ダブリー: 0)
+}, cities) => {
+	const prefCounts = {};
+	const prefs = cities.map(city => city.pref);
+	const prefsInHonshu = ['青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県']
+	for (let pref of prefs) {
+    if (!(pref in prefCounts)) { prefCounts[pref] = 1; }
+    else { prefCounts[pref]++; }
+	}
+	const maxRenpaku = Math.max(...Object.values(prefCounts));
+	const maxRenpakuPref = Object.keys(prefCounts).filter(pref => prefCounts[pref] === maxRenpaku).join(", ");
+
+	const doraChar = [];
+	for (let i = 0; i < kans.length + 1; i++) {
+		doraChar.push(...( dora[i].char || []) );
+		if (reachCount !== -1) { doraChar.push(...(uradora[i].char || [])); } 
+	}
+	const allTiles = cities.flatMap(city => city.tiles);
+	const doraCount = allTiles.filter(tile => doraChar.includes(tile.character)).length + allTiles.filter(tile => tile.red).length;
+
+	const yakuman = [];
+	if (cities.filter(city => city.seirei).length >= 5) {
+		if (cities.filter(city => !city.seirei).length === 0) {
+			yakuman.push({name: "清政令", point: 2});
+		}
+		else {
+			yakuman.push({name: "五政令", point: 1});
+		}
+	}
+	if (cities.filter(city => !city.kento).length === 0) {
+		yakuman.push({name: "清県都", point: 1});
+	}
+	if (cities.filter(city => city.ritou).length >= 5) {
+		if (cities.filter(city => !city.ritou).length === 0) {
+			yakuman.push({name: "清離島", point: 2});
+		}
+		else {
+			yakuman.push({name: "五離島", point: 1});
+		}
+	}
+	if (cities.filter(city => city.name.length === 4).length === 3) {
+		yakuman.push({name: "三槓子", point: 1});
+	}
+	if (cities.filter(city => city.name.length === 3).length === 4) {
+		yakuman.push({name: "四面子", point: 1});
+	}
+	if (cities.filter(city => city.name.length === 4).length === 4) {
+		yakuman.push({name: "四槓子", point: 2});
+	}
+	if (Object.keys(prefCounts).length === 1) {
+		yakuman.push({name: "清連泊", point: 1});
+	}
+
+	const yaku = [];
+	if (count === 0 && !isRinshan) {
+		yaku.push({name: "天和", point: 4});
+	}
+	if (reachCount !== -1) {
+		if (reachCount === 0) {
+			yaku.push({name: "二立直", point: 2});
+		}
+		else {
+			yaku.push({name: "立直", point: 1});
+		}
+	}
+	if (isRinshan) {
+		yaku.push({name: "嶺上", point: 1});
+	}
+	else {
+		if (reachCount !== -1 && count === reachCount + 1) {
+			yaku.push({name: "一発", point: 1});
+		}
+		if (count === MAX_COUNT - 1) {
+			yaku.push({name: "海底", point: 1});
+		}
+	}
+	yaku.push({name: "自摸", point: 1});
+	if (cities.filter(city => city.seirei).length === 2) {
+		yaku.push({name: "二政令", point: 1});
+	}
+	if (maxRenpaku === 2) {
+		yaku.push({name: `連泊 (${maxRenpakuPref})`, point: 1});
+	}
+	if (cities.filter(city => city.category !== "市").length === 0) {
+		yaku.push({name: "全市", point: 1});
+	}
+
+	if (cities.filter(city => city.kento).length === 3) {
+		yaku.push({name: "三県都", point: 2});
+	}
+	if (cities.filter(city => city.name.length === 3).length === 2) {
+		yaku.push({name: "二面子", point: 2});
+	}
+	if (maxRenpaku === 3) {
+		yaku.push({name: `三連泊 (${maxRenpakuPref})`, point: 2});
+	}
+	if (cities.filter(city => city.category !== "町").length === 0) {
+		yaku.push({name: "全町", point: 2});
+	}
+
+	if (cities.filter(city => city.seirei).length === 3) {
+		yaku.push({name: "三政令", point: 3});
+	}
+	if (cities.filter(city => city.ritou).length === 3) {
+		yaku.push({name: "三離島", point: 3});
+	}
+	if (cities.filter(city => city.name.length === 4).length === 1) {
+		yaku.push({name: "一槓子", point: 3});
+	}
+
+	if (cities.filter(city => city.kento).length === 4) {
+		yaku.push({name: "四県都", point: 4});
+	}
+	if (cities.filter(city => city.name.length === 3).length === 3) {
+		yaku.push({name: "三面子", point: 4});
+	}
+	if (maxRenpaku === 4) {
+		yaku.push({name: `四連泊 (${maxRenpakuPref})`, point: 4});
+	}
+	if (cities.filter(city => prefsInHonshu.includes(city.pref)).length === 0) {
+		yaku.push({name: "断本州", point: 4});
+	}
+	if (cities.filter(city => city.category !== "村").length === 0) {
+		yaku.push({name: "全村", point: 4});
+	}
+
+	if (cities.filter(city => city.seirei).length >= 4) {
+		yaku.push({name: "四政令", point: 6});
+	}
+	if (cities.filter(city => city.kento).length >= 5) {
+		yaku.push({name: "五県都", point: 6});
+	}
+	if (cities.filter(city => city.ritou).length >= 4) {
+		yaku.push({name: "四離島", point: 3});
+	}
+	if (cities.filter(city => city.name.length === 4).length === 2) {
+		yaku.push({name: "二槓子", point: 3});
+	}
+	if (maxRenpaku >= 5) {
+		yaku.push({name: `五連泊 (${maxRenpakuPref})`, point: 6});
+	}
+	if (doraCount) {
+		yaku.push({name: `ドラ ${doraCount}`, point: doraCount});
+	}
+
+	// 地一色: 未実装
+
+	const yakuCount = (yakuman.length? yakuman : yaku).reduce((a, b) => a + b.point, 0);
+
+	return yakuman.length? {
+			score: {
+				isYakuman: true,
+				point: 32000 * yakuCount,
+				text: "役満"
+			},
+			yaku: yakuman.map(yaku => ({
+				name: yaku.name,
+				point: yaku.point === 1? "役満" : "W役満"
+			}))
+		}　: {
+			score: {
+				isYakuman: false,
+				point: [1000, 2000, 4000, 8000, 8000, 12000, 12000, 16000, 16000, 16000, 24000, 24000][Math.min(yakuCount, 13) - 1],
+				text: yakuCount + "翻"
+			},
+			yaku: yaku.map(yaku => ({
+				name: yaku.name,
+				point: yaku.point + "翻"
+			}))
+		};
+};
--- src/scene/score.js
+++ src/scene/score.js
@@ -1,86 +1,92 @@
-const scoreScene = (tiles, cities, configToHandOver) => {
+const scoreScene = (game, cities, config) => {
 	$.reset();
-	const yaku = findYaku(tiles, cities);
-	let showCount = 1;
-
-	$.addItem([
-		{
-			hidden: () => $.disabled,
-			path: {
-				rect: [1100, 460, 400, 80],
-				radius: 40
-			},
-			draw: function() {
-				const [x, y, w, h] = this.path.rect;
-				$.ctx.bbFill(this.path, "#000"),
-				$.ctx.bbText("Twitter (現𝕏)", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle", color: "#ccc"});
-			},
-			onClick: () => {
-				const handStr = cities.flatMap(city => city.tiles).map(tile => tile.character).join("");
-				open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(handStr)}%20%23%E5%B8%82%E7%94%BA%E6%9D%91%E9%BA%BB%E9%9B%80&url=https%3A%2F%2Fbb.xrea.jp%2Fcitymj%2F`);
-			},
-			onHover: function() { $.ctx.bbFill(this.path, "rgba(255 255 255 / .3)"); }
-		},
-		{
-			hidden: () => $.disabled,
-			path: {
-				rect: [1100, 560, 400, 80],
-				radius: 40
-			},
-			draw: function() {
-				const [x, y, w, h] = this.path.rect;
-				$.ctx.bbFill(this.path, "#ccc"),
-				$.ctx.bbText("画像を保存", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle"});
-			},
-			onClick: () => {
-				const a = document.createElement("a");
-				a.href =  $.ctx.canvas.toDataURL("png");
-				a.download = "image.png";
-				a.click();
-			},
-			onHover: function() { $.ctx.bbFill(this.path, "rgba(0 0 0 / .1)"); }
-		},
-		{
-			hidden: () => $.disabled,
-			path: {
-				rect: [1100, 660, 400, 80],
-				radius: 40
-			},
-			draw: function() {
-				const [x, y, w, h] = this.path.rect;
-				$.ctx.bbFill(this.path, "#ccc"),
-				$.ctx.bbText("もう一度遊ぶ", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle"});
-			},
-			onClick: () => gameScene(configToHandOver),
-			onHover: function() { $.ctx.bbFill(this.path, "rgba(0 0 0 / .1)"); }
-		},
-		{
-			hidden: () => $.disabled,
-			path: { 
-				rect: [1100, 760, 400, 80],
-				radius: 40
-			},
-			draw: function() {
-				const [x, y, w, h] = this.path.rect;
-				$.ctx.bbFill(this.path, "#ccc"),
-				$.ctx.bbText("メニューに戻る", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle"});
-			},
-			onClick: () => menuScene(),
-			onHover: function() { $.ctx.bbFill(this.path, "rgba(0 0 0 / .1)"); }
-		}
-	]);
-	$.disabled = true;
+	const {yaku, score} = findYaku(game, cities);
+	let showCount = 0;
 
 	$.draw = () => {
 		$.ctx.bbFill({rect: [0, 0, 1600, 900]}, COLOR_BACKGROUND);
 		drawCityHands(cities);
 		drawCityNames(cities);
-		drawYaku(yaku, showCount, () => { showCount++; });
-		drawScore( yaku, showCount);
+		drawYaku(yaku, showCount);
+		if (showCount === yaku.length) {
+			$.ctx.bbText(`${score.text}`, 500, 800, {size: 40});
+			$.ctx.bbText(`${score.point}点`, 700, 760, {size: 80, color: score.isYakuman? "#f00" : COLOR_STRONG});
+		}
 		$.ctx.bbFill({rect: [450, 180, 2, 680]}, COLOR_STRONG);
 	};
 
-	$.update();
+	const increment = () => {
+		showCount++;
+		if (showCount < yaku.length) {
+			setTimeout(increment, 1000);
+		}
+		else {
+			$.addItem([
+				{
+					path: {
+						rect: [1100, 460, 400, 80],
+						radius: 40
+					},
+					draw: function() {
+						const [x, y, w, h] = this.path.rect;
+						$.ctx.bbFill(this.path, "#000"),
+						$.ctx.bbText("Twitter (現𝕏)", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle", color: "#ccc"});
+					},
+					onClick: () => {
+						const handStr = cities.flatMap(city => city.tiles).map(tile => tile.character).join("");
+						open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(handStr)}%20%23%E5%B8%82%E7%94%BA%E6%9D%91%E9%BA%BB%E9%9B%80&url=https%3A%2F%2Fbb.xrea.jp%2Fcitymj%2F`);
+					},
+					onHover: function() { $.ctx.bbFill(this.path, "rgba(255 255 255 / .3)"); }
+				},
+				{
+					path: {
+						rect: [1100, 560, 400, 80],
+						radius: 40
+					},
+					draw: function() {
+						const [x, y, w, h] = this.path.rect;
+						$.ctx.bbFill(this.path, "#ccc"),
+						$.ctx.bbText("画像を保存", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle"});
+					},
+					onClick: () => {
+						const a = document.createElement("a");
+						a.href =  $.ctx.canvas.toDataURL("png");
+						a.download = "image.png";
+						a.click();
+					},
+					onHover: function() { $.ctx.bbFill(this.path, "rgba(0 0 0 / .1)"); }
+				},
+				{
+					path: {
+						rect: [1100, 660, 400, 80],
+						radius: 40
+					},
+					draw: function() {
+						const [x, y, w, h] = this.path.rect;
+						$.ctx.bbFill(this.path, "#ccc"),
+						$.ctx.bbText("もう一度遊ぶ", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle"});
+					},
+					onClick: () => gameScene(config),
+					onHover: function() { $.ctx.bbFill(this.path, "rgba(0 0 0 / .1)"); }
+				},
+				{
+					path: { 
+						rect: [1100, 760, 400, 80],
+						radius: 40
+					},
+					draw: function() {
+						const [x, y, w, h] = this.path.rect;
+						$.ctx.bbFill(this.path, "#ccc"),
+						$.ctx.bbText("メニューに戻る", x + w / 2, y + 40, {size: 50, align: "center", baseline: "middle"});
+					},
+					onClick: () => menuScene(),
+					onHover: function() { $.ctx.bbFill(this.path, "rgba(0 0 0 / .1)"); }
+				}
+			]);
+		}
+		$.update();
+	};
+	increment();
 };
 
 const drawCityHands = (cities) => {
@@ -111,199 +117,12 @@ const drawCityNames = (cities) => {
 	}
 };
 
-const drawYaku = ({isYakuman, yaku}, count, increment) => {
+const drawYaku = (yaku, count) => {
 	const YAKU_X = 500;
 	const YAKU_Y = 200;
 	const YAKU_SIZE = 40;
-	if (isYakuman) {
-		for (let i = 0; i < count; i++) {
-			$.ctx.bbText(yaku[i].point === 1? "役満" : "W役満", YAKU_X, YAKU_Y + i * (YAKU_SIZE * 1.5), {size: YAKU_SIZE});
-			$.ctx.bbText(`${yaku[i].name}`, YAKU_X + 150, YAKU_Y + i * (YAKU_SIZE * 1.5), {size: YAKU_SIZE});
-		}
+	for (let i = 0; i < count; i++) {
+		$.ctx.bbText(yaku[i].point, YAKU_X, YAKU_Y + i * (YAKU_SIZE * 1.5), {size: YAKU_SIZE});
+		$.ctx.bbText(`${yaku[i].name}`, YAKU_X + 150, YAKU_Y + i * (YAKU_SIZE * 1.5), {size: YAKU_SIZE});
 	}
-	else {
-		for (let i = 0; i < count; i++) {
-			$.ctx.bbText(`${yaku[i].point}翻`, YAKU_X, YAKU_Y + i * (YAKU_SIZE * 1.5), {size: YAKU_SIZE});
-			$.ctx.bbText(`${yaku[i].name}`, YAKU_X + 100, YAKU_Y + i * (YAKU_SIZE * 1.5), {size: YAKU_SIZE});
-		}
-	}
-	if (count < yaku.length) {
-		setTimeout(() => {
-			increment();
-			$.update();
-		}, 1000);
-	}
-	else {
-		setTimeout(() => {
-			$.disabled = false;
-			$.update();
-		}, 1000)
-	}
-};
-
-const drawScore = ({isYakuman, yaku}) => {
-	if ($.disabled) { return; }
-	let scoreCount = yaku.reduce((a, b) => a + b.point, 0);
-	if (scoreCount >= 13) {
-		scoreCount = 1;
-		isYakuman = true;
-	}
-	const score = isYakuman
-		? 32000 * scoreCount
-		:	[1000, 2000, 4000, 8000, 8000, 12000, 12000, 16000, 16000, 16000, 24000, 24000][scoreCount - 1];
-	const SCORE_X = 500;
-	const SCORE_Y = 760;
-	const SCORE_SIZE = 80;
-	$.ctx.bbText(isYakuman? "役満" : `${scoreCount}翻`, SCORE_X, SCORE_Y + 40, {size: SCORE_SIZE / 2});
-	$.ctx.bbText(`${score}点`, SCORE_X + 200, SCORE_Y, {size: SCORE_SIZE, color: isYakuman? "#f00" : COLOR_STRONG});
-};
-
-const findYaku = (tiles, cities) => {
-	console.log(tiles)
-	const prefCounts = {};
-	const prefs = cities.map(city => city.pref);
-	const prefsInHonshu = ['青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県']
-	for (let pref of prefs) {
-    if (!(pref in prefCounts)) { prefCounts[pref] = 1; }
-    else { prefCounts[pref]++; }
-	}
-	const maxRenpaku = Math.max(...Object.values(prefCounts));
-	const maxRenpakuPref = Object.keys(prefCounts).filter(pref => prefCounts[pref] === maxRenpaku).join(", ");
-
-	const doraChar = [];
-	for (let i = 0; i < tiles.kans.length + 1; i++) {
-		doraChar.push(...( tiles.dora[i].char || []) );
-		if (tiles.reachCount !== -1) { doraChar.push(...(tiles.uradora[i].char || [])); } 
-	}
-	const allTiles = [...tiles.hand, ...tiles.kans.flat()];
-	const doraCount = allTiles.filter(tile => doraChar.includes(tile.character)).length + allTiles.filter(tile => tile.red).length;
-
-	const yakuman = [];
-	if (cities.filter(city => city.seirei).length >= 5) {
-		if (cities.filter(city => !city.seirei).length === 0) {
-			yakuman.push({name: "清政令", point: 2});
-		}
-		else {
-			yakuman.push({name: "五政令", point: 1});
-		}
-	}
-	if (cities.filter(city => !city.kento).length === 0) {
-		yakuman.push({name: "清県都", point: 1});
-	}
-	if (cities.filter(city => city.ritou).length >= 5) {
-		if (cities.filter(city => !city.ritou).length === 0) {
-			yakuman.push({name: "清離島", point: 2});
-		}
-		else {
-			yakuman.push({name: "五離島", point: 1});
-		}
-	}
-	if (cities.filter(city => city.name.length === 4).length === 3) {
-		yakuman.push({name: "三槓子", point: 1});
-	}
-	if (cities.filter(city => city.name.length === 3).length === 4) {
-		yakuman.push({name: "四面子", point: 1});
-	}
-	if (cities.filter(city => city.name.length === 4).length === 4) {
-		yakuman.push({name: "四槓子", point: 2});
-	}
-	if (Object.keys(prefCounts).length === 1) {
-		yakuman.push({name: "清連泊", point: 1});
-	}
-
-	const yaku = [];
-	if (tiles.count === 0 && !tiles.isRinshan) {
-		yaku.push({name: "天和", point: 4});
-	}
-	if (tiles.reachCount !== -1) {
-		if (tiles.reachCount === 0) {
-			yaku.push({name: "二立直", point: 2});
-		}
-		else {
-			yaku.push({name: "立直", point: 1});
-		}
-	}
-	if (tiles.isRinshan) {
-		yaku.push({name: "嶺上", point: 1});
-	}
-	else {
-		if (tiles.reachCount !== -1 && tiles.count === tiles.reachCount + 1) {
-			yaku.push({name: "一発", point: 1});
-		}
-		if (tiles.count === MAX_COUNT - 1) {
-			yaku.push({name: "海底", point: 1});
-		}
-	}
-	yaku.push({name: "自摸", point: 1});
-	if (cities.filter(city => city.seirei).length === 2) {
-		yaku.push({name: "二政令", point: 1});
-	}
-	if (maxRenpaku === 2) {
-		yaku.push({name: `連泊 (${maxRenpakuPref})`, point: 1});
-	}
-	if (cities.filter(city => city.category !== "市").length === 0) {
-		yaku.push({name: "全市", point: 1});
-	}
-
-	if (cities.filter(city => city.kento).length === 3) {
-		yaku.push({name: "三県都", point: 2});
-	}
-	if (cities.filter(city => city.name.length === 3).length === 2) {
-		yaku.push({name: "二面子", point: 2});
-	}
-	if (maxRenpaku === 3) {
-		yaku.push({name: `三連泊 (${maxRenpakuPref})`, point: 2});
-	}
-	if (cities.filter(city => city.category !== "町").length === 0) {
-		yaku.push({name: "全町", point: 2});
-	}
-
-	if (cities.filter(city => city.seirei).length === 3) {
-		yaku.push({name: "三政令", point: 3});
-	}
-	if (cities.filter(city => city.ritou).length === 3) {
-		yaku.push({name: "三離島", point: 3});
-	}
-	if (cities.filter(city => city.name.length === 4).length === 1) {
-		yaku.push({name: "一槓子", point: 3});
-	}
-
-	if (cities.filter(city => city.kento).length === 4) {
-		yaku.push({name: "四県都", point: 4});
-	}
-	if (cities.filter(city => city.name.length === 3).length === 3) {
-		yaku.push({name: "三面子", point: 4});
-	}
-	if (maxRenpaku === 4) {
-		yaku.push({name: `四連泊 (${maxRenpakuPref})`, point: 4});
-	}
-	if (cities.filter(city => prefsInHonshu.includes(city.pref)).length === 0) {
-		yaku.push({name: "断本州", point: 4});
-	}
-	if (cities.filter(city => city.category !== "村").length === 0) {
-		yaku.push({name: "全村", point: 4});
-	}
-
-	if (cities.filter(city => city.seirei).length >= 4) {
-		yaku.push({name: "四政令", point: 6});
-	}
-	if (cities.filter(city => city.kento).length >= 5) {
-		yaku.push({name: "五県都", point: 6});
-	}
-	if (cities.filter(city => city.ritou).length >= 4) {
-		yaku.push({name: "四離島", point: 3});
-	}
-	if (cities.filter(city => city.name.length === 4).length === 2) {
-		yaku.push({name: "二槓子", point: 3});
-	}
-	if (maxRenpaku >= 5) {
-		yaku.push({name: `五連泊 (${maxRenpakuPref})`, point: 6});
-	}
-	if (doraCount) {
-		yaku.push({name: `ドラ ${doraCount}`, point: doraCount});
-	}
-
-	// 地一色: 未実装
-	if (yakuman.length) { return { isYakuman: true, yaku: yakuman }; }
-	else { return { isYakuman: false, yaku }; }
 };
