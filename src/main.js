cfe5356057f1401c71faabc18260d2b1a2a745c6
k7ubb <64680945+k7ubb@users.noreply.github.com>
2024-10-15 01:12:05 +0900

    exact-canvasをcdn経由で使用

--- src/main.js
+++ src/main.js
@@ -7,9 +7,19 @@ const COLOR_STRONG = "#000080";
 const sleep = t => new Promise(resolve => setTimeout(resolve, t));
 const shuffle = arr => arr.sort(() => Math.random() - Math.random());
 
-const lib = new AutoresizeCanvas(document.getElementById("game"), 1600, 900, true);
-const $ = new ItemsCanvas(lib.context, lib.getMouseCoordinates, lib.isScaledPointInPath);
-lib.onResize = () => $.update();
+const exact = new ExactCanvas(document.getElementById("game"), 1600, 900, {rotate: true});
+const $ = new ItemsCanvas(
+	exact.canvas.getContext("2d"),
+	(event) => {
+		const [clientX, clientY] = event.type.includes('mouse')
+			? [event.clientX, event.clientY]
+			: [event.touches[0].clientX, event.touches[0].clientY];
+		const rect = event.target.getBoundingClientRect();
+		return exact.toLogicalPoint(clientX - rect.left, clientY - rect.top)
+	},
+	(path, x, y) => exact.canvas.getContext("2d").isPointInPath(path, ...exact.toPhysicalPoint(x, y))
+);
+exact.onResize = () => $.update();
 
 menuScene();
 /*
